1706bf2a9eac7412008b56e42bc158f883ce4e34
Sam <sam@arrowes.com.au>
2020-06-12 13:08:23 +1000

    initial commit

---
+++ src/app/identity.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IdentityService {
+
+public username : string = null;
+public loggedIn = false;
+
+  constructor() { }
+}
---
+++ src/app/model/entry.ts
@@ -0,0 +1,7 @@
+
+export class Entry {
+    id: number;
+    date: Date;
+    weight: number;
+    bodyfat: number;
+}
